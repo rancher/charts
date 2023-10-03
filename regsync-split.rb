6c355a90a6533f90cbfe3acbc1b0c98e3c0f12aa
Stefen Allen <sallen@suse.com>
2023-10-03 10:22:08 -0600

    Removing hidden characters in regsync-split.rb added ruby to GHA

--- regsync-split.rb
+++ regsync-split.rb
@@ -1,19 +1,19 @@
 #! /usr/bin/env ruby
-​
+
 require "json"
 require "pathname"
 require "yaml"
-​
+
 pwd = Pathname(Dir.pwd)
-​
+
 regsync = YAML.load((pwd + "regsync.yaml").read)
-​
+
 regsync["sync"].sum do |sync|
   sync["tags"]["allow"].count
 end.then do |sum|
   puts "total tags to consider: #{sum}"
 end
-​
+
 regsync["sync"].each do |sync|
   regsync.merge("sync" => [sync]).then do |regsync|
     (pwd + "regsync" + sync["source"]).then do |dir|
