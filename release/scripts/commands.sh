93510e43aa84cd2c543d7ace1b75de986b5af5f5
nicholasSUSE <nicholas.paolillo@suse.com>
2026-07-27 17:36:26 -0300

    feat(release): split commands output into ready and delayed charts
    Filter by ToRelease+QA+UnRC all true for ready; ToRelease true with QA or UnRC pending for delayed section.

--- release/scripts/commands.sh
+++ release/scripts/commands.sh
@@ -17,7 +17,21 @@ fi
 MINOR=$(basename "$FILE" .yaml | grep -oE '^[0-9]+\.[0-9]+')
 
 grep -v '<version>' "$FILE" | awk -v minor="$MINOR" '
-/^[a-z]/ { chart=$1; gsub(/:/, "", chart) }
+BEGIN { ri=0; di=0 }
+/^[a-z]/ { chart=$1; gsub(/:/, "", chart); torelease=0; qa=0; unrc=0 }
 /^  [^ ]/ { version=$1; gsub(/:/, "", version) }
-/ToRelease: true/ { print "release update charts " minor " " chart " " version }
+/ToRelease: true/ { torelease=1 }
+/QA: true/ { qa=1 }
+/UnRC: true/ { unrc=1 }
+/Released:/ {
+  if (torelease && qa && unrc) { ready[ri++] = "release update charts " minor " " chart " " version }
+  else if (torelease && (!qa || !unrc)) { delayed[di++] = "release update charts " minor " " chart " " version }
+}
+END {
+  for (i=0; i<ri; i++) print ready[i]
+  print ""
+  print "---------------------"
+  print "DELAYED CHARTS"
+  for (i=0; i<di; i++) print delayed[i]
+}
 '
