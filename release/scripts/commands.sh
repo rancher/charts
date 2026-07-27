e8a72fc3cd6e729ca41a3d5b0f6efdbfc5c2695b
nicholasSUSE <nicholas.paolillo@suse.com>
2026-07-27 17:59:58 -0300

    feat(release): validate chart versions against remote dev branch
    Add GitHub API check per chart; charts missing from dev-v<minor> or
    lacking QA/UnRC approval are grouped under PROBLEM CHARTS.

--- release/scripts/commands.sh
+++ release/scripts/commands.sh
@@ -15,23 +15,36 @@ fi
 
 # Extract minor version from filename: 2.15.0.yaml -> 2.15
 MINOR=$(basename "$FILE" .yaml | grep -oE '^[0-9]+\.[0-9]+')
+BRANCH="dev-v${MINOR}"
 
-grep -v '<version>' "$FILE" | awk -v minor="$MINOR" '
-BEGIN { ri=0; di=0 }
+READY_FILE=$(mktemp)
+PROBLEM_FILE=$(mktemp)
+trap 'rm -f "$READY_FILE" "$PROBLEM_FILE"' EXIT
+
+grep -v '<version>' "$FILE" | awk -v minor="$MINOR" -v ready="$READY_FILE" -v problem="$PROBLEM_FILE" '
+BEGIN { torelease=0; qa=0; unrc=0 }
 /^[a-z]/ { chart=$1; gsub(/:/, "", chart); torelease=0; qa=0; unrc=0 }
 /^  [^ ]/ { version=$1; gsub(/:/, "", version) }
 /ToRelease: true/ { torelease=1 }
 /QA: true/ { qa=1 }
 /UnRC: true/ { unrc=1 }
 /Released:/ {
-  if (torelease && qa && unrc) { ready[ri++] = "release update charts " minor " " chart " " version }
-  else if (torelease && (!qa || !unrc)) { delayed[di++] = "release update charts " minor " " chart " " version }
-}
-END {
-  for (i=0; i<ri; i++) print ready[i]
-  print ""
-  print "---------------------"
-  print "DELAYED CHARTS"
-  for (i=0; i<di; i++) print delayed[i]
+  if (torelease && qa && unrc) print "release update charts " minor " " chart " " version >> ready
+  else if (torelease && (!qa || !unrc)) print "release update charts " minor " " chart " " version >> problem
 }
 '
+
+while IFS= read -r cmd; do
+  chart=$(echo "$cmd" | awk '{print $5}')
+  version=$(echo "$cmd" | awk '{print $6}')
+  if gh api "repos/rancher/charts/contents/charts/${chart}/${version}?ref=${BRANCH}" &>/dev/null 2>&1; then
+    echo "$cmd"
+  else
+    echo "$cmd" >> "$PROBLEM_FILE"
+  fi
+done < "$READY_FILE"
+
+echo ""
+echo "---------------------"
+echo "PROBLEM CHARTS"
+cat "$PROBLEM_FILE"
